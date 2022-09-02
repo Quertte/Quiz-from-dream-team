a0eb83a46d72d4ad27ffefa4314539d13b776d98
serkoo10 <sercoo1407@icloud.com>
2022-09-02 12:09:24 +0300

    вытащил вопросы по айдишнику темы

--- routers/routerThemes.js
+++ routers/routerThemes.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const ThemesPage = require('../View/ThemesPage')
+const express = require('express');
+const ThemesPage = require('../View/ThemesPage');
+const { Question } = require('../db/models');
+
 const themePageRouter = express.Router();
-const { Theme } = require('../db/models')
+const { Theme } = require('../db/models');
 
-themePageRouter.get('/', async(req,res) => {
-    const themeArr = await Theme.findAll();
+themePageRouter.get('/', async (req, res) => {
+  const themeArr = await Theme.findAll();
 
-  res.renderComponent(ThemesPage,  { themeArr })
-})
+  res.renderComponent(ThemesPage, { themeArr });
+});
+themePageRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const questions = await Question.findAll({ where: { theme_id: id } });
+res.renderComponent()
+});
 
-module.exports = themePageRouter
+module.exports = themePageRouter;
