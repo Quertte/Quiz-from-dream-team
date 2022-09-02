b021123e1ed35745af537b75c182a478edbb22e3
Vitalik28 <vitalik.ter28@gmail.com>
2022-09-02 14:12:02 +0300

    release1

--- View/Card.jsx
+++ View/Card.jsx
@@ -2,7 +2,7 @@ const React = require('react');
 
 module.exports = function Card({ thema }) {
   return (
-    <div className = 'theme'><a href='#' className={thema.id} style={{color:'white'}}>
+    <div ><a href='#' className={thema.id} style={{color:'white'}}>
         { thema.title }
       </a></div>
       
